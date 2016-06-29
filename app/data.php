c06e31329504a1ca9311ee986e9e7af8e543edbf
ak900s <ak900s@yahoo.com.mx>
2016-06-29 01:11:18 -0500

    update

--- app/data.php
+++ app/data.php
@@ -121,7 +121,15 @@ class app_data {
     return $hex;
     }
     
-    
+    public function hexToStr($hex)
+{
+    $string='';
+    for ($i=0; $i < strlen($hex)-1; $i+=2)
+    {
+        $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+    }
+    return $string;
+}
     
     //put your code here
 }
