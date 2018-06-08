9f0fdad2d44db2e5b5e1d90c9a5e0373ea02644d
adrian <aromero@orionworkflow.net>
2018-06-08 16:37:39 -0500

    update

--- block/whatss.php
+++ block/whatss.php
@@ -3,17 +3,60 @@
 $parameters= $this->getNet('parameters');
 
 
-
+$string="";
 
 if(isset($parameters['request'])){
 foreach($parameters['request'] as $key=>$param){
-var_dump($key);
-var_dump($param);
+
+if(is_array($param)){
+
+foreach($param as $k=>$value){
+$string = $string.'\n'.$k.'::'.$value;
+}
+
+}
+else{
+$string = $string.'\n'.$key.':'.$param;
+}
 
 
 }
 }
+if($string!=''){
+file_put_contents('./data/request.txt', $string);
+}
 
-echo "whats app";
+
+
 
 ?>
+
+<form action="https://www.waboxapp.com/api/send/chat" id="send" method="get" >
+
+<span>
+<p>token</p>
+<input name="token" value="" />
+<br>
+<p>uid</p>
+<input name="uid" value="" />
+<br>
+<p>custom uid</p>
+<input name="custom_uiid" value="" />
+<br>
+</span>
+<br>
+<span>
+<p>Numero</p>
+<input name="to" value="" />
+</span>
+<br>
+<span>
+<p>Mensaje</p>
+<input name="text" value="" />
+</span>
+<br>
+
+<input type="submit" value="enviar"  >
+
+
+</form>
