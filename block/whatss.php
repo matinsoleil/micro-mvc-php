6544fc6534e060995ccd17153d4b7405a980dc81
adrian <aromero@orionworkflow.net>
2018-06-08 11:41:13 -0500

    match

--- block/whatss.php
+++ block/whatss.php
@@ -3,6 +3,14 @@
 $parameters= $this->getNet('parameters');
 
 
+foreach($_POST as $parammeter){
+
+echo $parammeter.' ';
+
+
+
+}
+
 if(isset($parameters['request'])){
 foreach($parameters['request'] as $key=>$param){
 
