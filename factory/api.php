87ede8a6744f808fd9ed382f4051b496ac0c0936
adrian <aromero@orionworkflow.net>
2018-08-14 18:17:03 -0500

    update

--- factory/api.php
+++ factory/api.php
@@ -10,16 +10,32 @@ public $net;
 
 
      $action=$this->net['action']->uri;
+     fopen("http://someurl/file.zip", 'r');
 
      $data = array("id"=>"2","request"=>"true");
      header('Content-Type: application/json');
      echo json_encode($data);
 
-
+     this->check();
 
 
     }
-    public function block($entity){
+    public function check(){
+
+	$urls= array(
+	"https://www.consejomexcardio.org/",
+	"https://www.consejomexcardio.org/Historia",
+	"https://www.consejomexcardio.org/Cursos",
+	"https://www.consejomexcardio.org/Regulacion",
+	"https://www.consejomexcardio.org/Certificacion",
+	"https://www.consejomexcardio.org/Directorio",
+	"https://www.consejomexcardio.org/Avisos",
+	"https://www.consejomexcardio.org/Contacto"
+	);
+
+      foreach($urls as $url){
+      file_get_contents($url);
+      }
 
 
     }
